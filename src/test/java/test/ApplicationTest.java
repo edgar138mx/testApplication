42277760a4664399715df3629cfc18f4909f8bde
edgar138mx <edgar138mx@gmail.com>
2017-08-21 17:43:02 -0300

    Add files via upload

---
+++ src/test/java/test/ApplicationTest.java
@@ -0,0 +1,35 @@
+package test;
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+
+
+public class ApplicationTest {
+	private WebDriver driver;
+
+	@Test          
+    public void testPetClinic() {    
+//Change the URL based on the location where Tomcat is installed
+driver.get("http://localhost:8081/petclinic/");    
+      String title = driver.getTitle();      
+Assert.assertTrue(title.contains("a Spring Frameworkk"));   
+    }
+
+	@BeforeTest
+	public void beforeTest() {
+		File file = new File("D:\\instaladores\\gecko\\geckodriver.exe");
+		System.setProperty("webdriver.gecko.driver", file.getAbsolutePath());
+		driver = new FirefoxDriver();
+	}
+
+	@AfterTest
+	public void afterTest() {
+		driver.quit();
+	}
+}
